a6d386711fd982e7f5213d34b0367fef5f017dd5
mklau <matthewklau@fas.harvard.edu>
2016-12-19 16:07:38 -0500

    updates data_types.R

--- src/data_types.R
+++ src/data_types.R
@@ -1,9 +1,12 @@
-                                        #
-add <- read.csv('../data/addme.csv',header=F)
-x <- read.csv('../data/x.csv',header=F)
-y <- read.csv('../data/y.csv',header=F)
-                                        #
+### "add": has header, all values quoted
+### has the letter "l" in line 9
+add <- read.csv('../data/addme.csv',header=T)
 c(apply(add,2,mode),apply(add,2,class))
+### "x": no header, no values quoted
+### has the letter "l" in line 9
+x <- read.csv('../data/x.csv',header=F)
 c(apply(x,2,mode),apply(x,2,class))
+### "y": no header, no values quoted
+### only numbers
+y <- read.csv('../data/y.csv',header=F)
 c(apply(y,2,mode),apply(y,2,class))
-
